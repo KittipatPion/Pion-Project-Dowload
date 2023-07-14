0347b0382ae2f96c244df667546325fd56fb00fe
KittipatPion <kittipat@pion.co.th>
2023-07-14 13:14:25 +0700

    game

---
+++ WebApplication1/Pages/EQC.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class EQCModel : PageModel
+    {
+        private readonly ILogger<EQCModel> _logger;
+
+        public EQCModel(ILogger<EQCModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApplication1/Pages/WMXUAT.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class WMXModel : PageModel
+    {
+        private readonly ILogger<WMXModel> _logger;
+
+        public WMXModel(ILogger<WMXModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
